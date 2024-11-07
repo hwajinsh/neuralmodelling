ef554a375e74454e9d5d39ac4d5080f91e9e95ad
luciaganton <80822413+luciaganton@users.noreply.github.com>
2024-11-07 18:24:48 +0100

    f

--- explainingaway.py
+++ explainingaway.py
@@ -5,41 +5,60 @@ from rescorla_wagner import rescorla_wagner
 ## Explaining Away
 
 # Parameters for the Rescorla-Wagner model
-epsilon = 0.1
+epsilon = 0.15
 num_trials_pretraining = 100 
 num_trials_training = 100    
-total_trials = num_trials_pretraining + num_trials_training 
+CS1_testing = 1
+CS2_testing = 1      
+total_trials = num_trials_pretraining + num_trials_training + CS1_testing + CS2_testing
 
-stimuli_1 = np.concatenate([np.ones(num_trials_pretraining), np.ones(num_trials_training)])
-stimuli_2 = np.concatenate([np.ones(num_trials_pretraining), np.zeros(num_trials_training)])
+
+stimuli_1 = np.concatenate([np.ones(num_trials_pretraining), np.ones(num_trials_training), np.ones(CS1_testing), np.zeros(CS2_testing)])
+stimuli_2 = np.concatenate([np.ones(num_trials_pretraining), np.zeros(num_trials_training), np.zeros(CS1_testing), np.ones(CS2_testing)])
+
 
 # Reward array (1 = reward, 0 = no reward in the Result Phase)
 rewards = np.concatenate([np.ones(num_trials_pretraining), np.ones(num_trials_training)])
 
-# Ideal expectations in blocking: reward is always present
-ideal_expectations = np.concatenate([np.ones(num_trials_pretraining), np.ones(num_trials_training)])
-
 # Apply Rescorla-Wagner rule
 predictions_v, weights_1, weights_2 = rescorla_wagner(stimuli_1, stimuli_2, rewards, epsilon)
 
-# Plot: Learned predictions and ideal expectations
+# Testing
+testing_1 = weights_1[total_trials - 3] * stimuli_1[total_trials - 2] + weights_2[total_trials - 3] * stimuli_2[total_trials - 2]
+testing_2 = weights_1[total_trials - 3] * stimuli_1[total_trials - 1] + weights_2[total_trials - 3] * stimuli_2[total_trials - 1]
+
+#CHANGEEEEEEEEEEEE
+# Ideal expectations in blocking: RW model correctly reflects the idealised expectations
+ideal_expectations = predictions_v
+ideal_weight_1 = weights_1
+ideal_weight_2 = weights_2
+ideal_test_1 = testing_1
+ideal_test_2 = testing_2
+
+# Plot 1: Idealised Expectations for Explaining Away Across Trials
 plt.figure(figsize=(10, 5))
-plt.plot(predictions_v, label="Learned Predictions", color="blue")
-plt.plot(ideal_expectations, label="Ideal Expectations", color="orange")
+plt.plot(predictions_v, label="Summed Idealised Expectations", color="green", linestyle = "--")
+plt.plot(ideal_weight_1, label="Ideal Expectations (Stimulus 1)", color="blue")
+plt.plot(ideal_weight_2, label="Ideal Expectations (Stimulus 2)", color="orange")
+plt.plot((total_trials - 2), ideal_test_1, 'o', color="blue")
+plt.plot((total_trials - 1), ideal_test_2, 'o', color="orange")
 plt.xlabel("Trials")
 plt.ylabel("Values")
-plt.title("Learned Predictions and Ideal Expectations")
+plt.title("Idealised Expectations for Explaining Away Across Trials")
 plt.legend()
 plt.grid(True)
 plt.show()
 
-# Plot 2: Expectations (Weights) for both stimuli
+# Plot 2: Learned Expectations for Explaining Away Across Trials
 plt.figure(figsize=(10, 5))
-plt.plot(weights_1, label="Weight for Stimulus 1", color="blue")
-plt.plot(weights_2, label="Weight for Stimulus 2", color="orange", linestyle="--")
+plt.plot(predictions_v, label="Summed Learned Expectations", color="green", linestyle = "--")
+plt.plot(weights_1, label="Weight (Stimulus 1)", color="blue")
+plt.plot(weights_2, label="Weight (Stimulus 2)", color="orange")
+plt.plot((total_trials - 2), testing_1, 'o', color="blue")
+plt.plot((total_trials - 1), testing_2, 'o', color="orange")
 plt.xlabel("Trials")
 plt.ylabel("Weights")
-plt.title("Expectations (Weights) for Stimulus 1 and Stimulus 2")
+plt.title("Learned Expectations for Explaining Away Across Trials")
 plt.legend()
 plt.grid(True)
 plt.show()
--- inhibitoryconditioning.py
+++ inhibitoryconditioning.py
@@ -7,36 +7,60 @@ from rescorla_wagner import rescorla_wagner
 ## We need more trials in training and testing (specially in training) so that it
 # does not fluctuate in the beginning and it stabilizes
 
-# Parameters for the Rescorla-Wagner model
-epsilon = 0.1  # Learning rate for stimuli (CS1, CS2)
-num_trials_training = 200     # Trials in Training (CS1 + CS2)
-#num_trials_result = 200     # Trials in Result phase for visualizing expectations
-total_trials = num_trials_training 
+epsilon = 0.4
+num_trials_training = 100    
+CS1_testing = 1
+CS2_testing = 1      
+total_trials = num_trials_training + CS1_testing + CS2_testing
 
-stimuli_1 = np.concatenate([np.ones(num_trials_training)])
+stimuli_1 = np.concatenate([np.ones(num_trials_training), np.ones(CS1_testing), np.zeros(CS2_testing)])
 stimuli_2 = np.concatenate([
-    np.tile([1, 0], num_trials_training // 2) # CS2 is present only when CS1 is not rewarded
+    np.tile([1, 0], num_trials_training // 2), np.zeros(CS1_testing), np.ones(CS2_testing) # CS2 is present only when CS1 is not rewarded
 ])
 
-#stimuli_2 = np.ones(total_trials)
-#stimuli_2[1::2] = 0 
 
 # Reward array (difference between stimuli_1 and stimuli_2 or inhibitory stimuli)
 rewards = stimuli_1 - stimuli_2
 
-# Ideal expectations in binhibitory conditioning: (difference between stimuli_1 and stimuli_2 or inhibitory stimuli)
-ideal_expectations = stimuli_1 - stimuli_2
-
 # Apply Rescorla-Wagner rule
 predictions_v, weights_1, weights_2 = rescorla_wagner(stimuli_1, stimuli_2, rewards, epsilon)
 
-# Plot expectations (Weights) for both stimuli
+# Testing
+testing_1 = weights_1[total_trials - 3] * stimuli_1[total_trials - 2] + weights_2[total_trials - 3] * stimuli_2[total_trials - 2]
+testing_2 = weights_1[total_trials - 3] * stimuli_1[total_trials - 1] + weights_2[total_trials - 3] * stimuli_2[total_trials - 1]
+
+# Ideal expectations in Inhibitory Conditioning: RW model correctly reflects the idealised expectations
+ideal_expectations = predictions_v
+ideal_weight_1 = weights_1
+ideal_weight_2 = weights_2
+ideal_test_1 = testing_1
+ideal_test_2 = testing_2
+
+
+# Plot 1: Idealised Expectations for Inhibitory Conditioning Across Trials
 plt.figure(figsize=(10, 5))
-plt.plot(weights_1, label="Weight for Stimulus 1", color="blue")
-plt.plot(weights_2, label="Weight for Stimulus 2", color="orange")
+plt.plot(predictions_v, label="Summed Idealised Expectations", color="green", linestyle = "--")
+plt.plot(ideal_weight_1, label="Ideal Expectations (Stimulus 1)", color="blue")
+plt.plot(ideal_weight_2, label="Ideal Expectations (Stimulus 2)", color="orange")
+plt.plot((total_trials - 2), ideal_test_1, 'o', color="blue")
+plt.plot((total_trials - 1), ideal_test_2, 'o', color="orange")
+plt.xlabel("Trials")
+plt.ylabel("Values")
+plt.title("Idealised Expectations for Inhibitory Conditioning Across Trials")
+plt.legend()
+plt.grid(True)
+plt.show()
+
+# Plot 2: Learned Expectations for Inhibitory Conditioning Across Trials
+plt.figure(figsize=(10, 5))
+plt.plot(predictions_v, label="Summed Learned Expectations", color="green", linestyle = "--")
+plt.plot(weights_1, label="Weight (Stimulus 1)", color="blue")
+plt.plot(weights_2, label="Weight (Stimulus 2)", color="orange")
+plt.plot((total_trials - 2), testing_1, 'o', color="blue")
+plt.plot((total_trials - 1), testing_2, 'o', color="orange")
 plt.xlabel("Trials")
 plt.ylabel("Weights")
-plt.title("Expectations (Weights) for Stimulus 1 and Stimulus 2")
+plt.title("Learned Expectations for Inhibitory Conditioning Across Trials")
 plt.legend()
 plt.grid(True)
 plt.show()
